533703a14d5e0a881a86c6b83255415adea5ff14
Ehtesham-OX <ehtesham.ox@gmail.com>
2021-02-10 16:34:35 +0330

    component & loop & events

---
+++ src/components/Twoots.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>{{ twoot.id }} - {{ twoot.title }}</h3>
+        <h4 v-if="twoot.isFavo">IS FAV!</h4>
+        <p>{{ twoot.content }}</p>
+        <button @click="twootClicked(twoot.id)">Just click Me!</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Twoots',
+    props: { 
+        twoot: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        twootClicked(id) {
+            this.$emit('clicked', id);
+        }
+    },
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,75 @@
+<template>
+    @{{ user.userName }} ---- {{ fullName }}
+    <br>
+    <strong>Followers: {{ followers }}</strong>
+    <br>
+    <button @click="incrementFollower">Be My Follower</button>
+    <hr>
+    <Twoots v-for="twoot in user.twoots"
+            :key="twoot.id" 
+            :twoot="twoot"
+            @clicked="showTwootClicked"
+    />
+</template>
+
+<script>
+import Twoots from './Twoots';
+
+export default {
+  name: 'UserProfile',
+  components: {
+        Twoots
+  },
+  data () {
+    return {
+        followers: 0,
+        user: {
+            userName: 'Ehtesham-OX',
+            firstName: 'Ehtesham',
+            lastName: 'Zahraei',
+            email: 'ehtesham.ox@gmail.com',
+            isAdmin: true,
+            twoots: [
+                { id:1, title:'Che khabar?', content:'Hello Mammad!', isFavo: false },
+                { id:2, title:'Khobi?', content:'Hello Asghar!', isFavo: false },
+            ]
+        } 
+    }
+  },
+  watch: {
+    followers(oldFollower, newFollower) {
+      if(oldFollower !== newFollower)
+        console.log(`${this.user.userName} has gained the follower!`);
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    incrementFollower() {
+        this.followers++;
+    },
+    showTwootClicked(id) {
+        this.user.twoots.forEach(twoot => {
+            if(twoot.id === id )
+                twoot.isFavo = true;
+            else 
+                twoot.isFavo = false;
+        });
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
